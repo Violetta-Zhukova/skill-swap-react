0505010dabe8c8c220419a0857c03f63d8cc952b
DmitryChesnov <advokat33333@gmail.com>
2025-12-01 22:43:52 +0300

    Изменение на модуль css

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import { RadioGroup } from "../shared/ui/RadioGroup/RadioGroup";
+import type { OptionType } from "../shared/ui/RadioGroup/Option";
+
 function App() {
-  return <div></div>;
+  const options = [
+    {
+      title: "Не имеет значения",
+      value: "any",
+    },
+    {
+      title: "Мужской",
+      value: "male",
+    },
+    {
+      title: "Женский",
+      value: "female",
+    },
+  ];
+  
+  const [state, setState] = useState(options[0]);
+  
+  const handleChange = (option: OptionType) => {
+    setState(option)
+    console.log(option)
+  }
+
+  return (
+    <div>
+      <RadioGroup 
+        name="authorSex" 
+        onChange={handleChange} 
+        options={options} 
+        selected={state} 
+        title="Пол автора"
+      />
+    </div>
+  );
 }
 
 export default App;
