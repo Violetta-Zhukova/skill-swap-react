db91ab5896838c903479ab75cbc7cf5da0b50408
Violetta Zhukova <puzanova.violetta@gmail.com>
2025-12-01 20:05:20 +0300

    feat: добавлен компонент modal-confirm и его тестовое отображение в App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,19 @@
+import { ModalConfirm } from "../widgets/modal-confirm/modal-confirm";
+
 function App() {
-  return <div></div>;
+  return (
+    <div style={{ width: "556px", height: "420px" }}>
+      <ModalConfirm
+        image=""
+        title={"Ваше предложение создано"}
+        text={"Теперь вы можете предложить обмен"}
+        buttonText={"Готово"}
+        onClose={() => {
+          console.log("Нажали на кнопку закрыть");
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/widgets/modal-confirm/modal-confirm.tsx
@@ -0,0 +1,27 @@
+import type { FC } from "react";
+import styles from "./modal-confirm.module.css";
+
+type TModalConfirmProps = {
+  image: string;
+  title: string;
+  text: string;
+  buttonText: string;
+  onClose: () => void;
+};
+
+export const ModalConfirm: FC<TModalConfirmProps> = ({
+  image,
+  title,
+  text,
+  buttonText,
+  onClose,
+}) => (
+  <>
+    <img src={image} className={styles.image} />
+    <h2 className={styles.title}>{title}</h2>
+    <p className={styles.content}>{text}</p>
+    <button onClick={onClose} className={styles.button}>
+      {buttonText}
+    </button>
+  </>
+);
