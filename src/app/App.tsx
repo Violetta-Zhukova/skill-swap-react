a66499ab693f9517565f89d4442430e27cf91009
Ekaterina Novikova <fearnocait@gmail.com>
2025-12-03 14:30:41 +0300

    [misc] удалили тест данные

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,36 +1,5 @@
-import { useEffect, useState } from "react";
-import type { User, SkillCategory } from "../entities/types";
-import { api } from "../api";
-import { UserCard } from "../widgets/UserCard/UserCard";
-import styles from "./App.module.css";
-
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [categories, setCategories] = useState<SkillCategory[]>([]);
-
-  useEffect(() => {
-    const loadData = async () => {
-      const [usersData, categoriesData] = await Promise.all([
-        api.getAllUsers(),
-        api.getCategories(),
-      ]);
-
-      setUsers(usersData);
-      setCategories(categoriesData);
-    };
-
-    loadData();
-  }, []);
-
-  return (
-    <main className={styles.page}>
-      <section className={styles.cardsGrid}>
-        {users.map((user) => (
-          <UserCard key={user.id} user={user} categories={categories} />
-        ))}
-      </section>
-    </main>
-  );
+  return <div></div>;
 }
 
 export default App;
